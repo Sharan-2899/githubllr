ec7d4553291827eb23a340d9ce614ccd2e447057
Sharan-2899 <totti031999@gmail.com>
2025-09-23 21:26:57 +0530

    new file added

---
+++ if_elsetask.py
@@ -0,0 +1,17 @@
+# Task 10
+
+look=str(input("watch for the signal and respond and move according: ")) 
+
+if look=="red":
+  print("stop! and wait")
+
+elif look=="yellow":
+    print("get ready!")
+
+elif look=="green":
+    print("Go!")
+
+else:
+    print("************************************************************")
+    print("Watch carefully ! your'e not paying attention to the signal!")  
+    print("************************************************************")  
